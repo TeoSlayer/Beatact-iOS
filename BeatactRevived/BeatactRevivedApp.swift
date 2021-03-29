6c45a1a0725c58535cb684ea8c7b085812cbbbea
Teo Slayer <teodor.calin@me.com>
2021-03-30 00:13:15 +0300

    Initial Commit

---
+++ BeatactRevived/BeatactRevivedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatactRevivedApp.swift
+//  BeatactRevived
+//
+//  Created by Calin Teodor on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BeatactRevivedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
